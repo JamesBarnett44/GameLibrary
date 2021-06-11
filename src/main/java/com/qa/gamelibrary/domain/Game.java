a64f98972e6dcea6ce4f703a3af540a93e7a0a33
Jamie Barnett <jamiebarnett44@gmail.com>
2021-06-11 14:45:21 +0100

    GL-10 GL-13 GL-18 GL-12 GL-14 JB: Backend CRUD functionality for Game

--- src/main/java/com/qa/gamelibrary/domain/Game.java
+++ src/main/java/com/qa/gamelibrary/domain/Game.java
@@ -14,7 +14,7 @@ public class Game {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@Column(name = "Name", nullable = false)
+	@Column(name = "Name")
 	private String name;
 
 	@Column(name = "Genre")
@@ -23,29 +23,29 @@ public class Game {
 	@Column(name = "Progress")
 	private String progress;
 
-	@Column(name = "Platform", nullable = false)
-	private int platformId;
+//	@Column(name = "Platform")
+//	private int platformId;
 	
-	@ManyToOne
-	private Platform platform;
+//	@ManyToOne
+//	private Platform platform;
 
 	public Game() {
 
 	}
 
-	public Game(Integer id, String name, String genre, String progress, Integer platformId) {
+	public Game(Integer id, String name, String genre, String progress) {
 		this.id = id;
 		this.name = name;
 		this.genre = genre;
 		this.progress = progress;
-		this.platformId = platformId;
+		//this.platformId = platformId;
 	}
 
-	public Game(String name, String genre, String progress, Integer platformId) {
+	public Game(String name, String genre, String progress) {
 		this.name = name;
 		this.genre = genre;
 		this.progress = progress;
-		this.platformId = platformId;
+		//this.platformId = platformId;
 	}
 
 	public Integer getId() {
@@ -79,29 +79,37 @@ public class Game {
 	public void setProgress(String progress) {
 		this.progress = progress;
 	}
+	
+	
 
-	public int getPlatformId() {
-		return platformId;
-	}
+//	public int getPlatformId() {
+//		return platformId;
+//	}
+//
+//	public void setPlatformId(Integer platformId) {
+//		this.platformId = platformId;
+//	}
 
-	public void setPlatformId(Integer platformId) {
-		this.platformId = platformId;
-	}
+//	public Platform getPlatform() {
+//		return platform;
+//	}
+//
+//	public void setPlatform(Platform platform) {
+//		this.platform = platform;
+//	}
 
 	@Override
 	public String toString() {
-		return "Game [Id=" + id + ", name=" + name + ", genre=" + genre + ", progress=" + progress + ", platformId="
-				+ platformId + "]";
+		return "Game [id=" + id + ", name=" + name + ", genre=" + genre + ", progress=" + progress + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + platformId;
 		result = prime * result + ((progress == null) ? 0 : progress.hashCode());
 		return result;
 	}
@@ -115,22 +123,20 @@ public class Game {
 		if (getClass() != obj.getClass())
 			return false;
 		Game other = (Game) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
 		if (genre == null) {
 			if (other.genre != null)
 				return false;
 		} else if (!genre.equals(other.genre))
 			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
 		} else if (!name.equals(other.name))
-			return false;
-		if (platformId != other.platformId)
 			return false;
 		if (progress == null) {
 			if (other.progress != null)
--- src/main/java/com/qa/gamelibrary/dto/GameDTO.java
+++ src/main/java/com/qa/gamelibrary/dto/GameDTO.java
@@ -1,30 +1,32 @@
 package com.qa.gamelibrary.dto;
 
+import com.qa.gamelibrary.domain.Game;
+
 public class GameDTO {
 
 	private Integer id;
 	private String name;
 	private String genre;
 	private String progress;
-	private int platformId;
+	//private int platformId;
 
 	public GameDTO() {
 
 	}
 
-	public GameDTO(Integer id, String name, String genre, String progress, Integer platformId) {
+	public GameDTO(Integer id, String name, String genre, String progress) {
 		this.id = id;
 		this.name = name;
 		this.genre = genre;
 		this.progress = progress;
-		this.platformId = platformId;
+		//this.platformId = platformId;
 	}
 
-	public GameDTO(String name, String genre, String progress, Integer platformId) {
+	public GameDTO(String name, String genre, String progress) {
 		this.name = name;
 		this.genre = genre;
 		this.progress = progress;
-		this.platformId = platformId;
+		//this.platformId = platformId;
 	}
 
 	public Integer getId() {
@@ -59,28 +61,26 @@ public class GameDTO {
 		this.progress = progress;
 	}
 
-	public int getPlatformId() {
-		return platformId;
-	}
-
-	public void setPlatformId(Integer platformId) {
-		this.platformId = platformId;
-	}
+//	public int getPlatformId() {
+//		return platformId;
+//	}
+//
+//	public void setPlatformId(Integer platformId) {
+//		this.platformId = platformId;
+//	}
 
 	@Override
 	public String toString() {
-		return "Game [Id=" + id + ", name=" + name + ", genre=" + genre + ", progress=" + progress + ", platformId="
-				+ platformId + "]";
+		return "GameDTO [id=" + id + ", name=" + name + ", genre=" + genre + ", progress=" + progress + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + platformId;
 		result = prime * result + ((progress == null) ? 0 : progress.hashCode());
 		return result;
 	}
@@ -94,22 +94,20 @@ public class GameDTO {
 		if (getClass() != obj.getClass())
 			return false;
 		GameDTO other = (GameDTO) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
 		if (genre == null) {
 			if (other.genre != null)
 				return false;
 		} else if (!genre.equals(other.genre))
 			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
 		} else if (!name.equals(other.name))
-			return false;
-		if (platformId != other.platformId)
 			return false;
 		if (progress == null) {
 			if (other.progress != null)
