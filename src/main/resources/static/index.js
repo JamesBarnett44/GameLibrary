506e1b8daa6baae90803c1844a99363928f547d8
Jamie Barnett <jamiebarnett44@gmail.com>
2021-06-15 17:39:35 +0100

    GL-10 GL-13 GL-14 JB: Added create, read and delete functionality for Games from the frontend

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -4,40 +4,21 @@
 
 
 //let f = document.getElementById("gameForm");
-//CREATE
-document.querySelector("#gameForm").addEventListener("submit", function (event) {
-    event.preventDefault();
 
-    const data = {
-        name: this.gameName.value,
-        //platform: this.platform.value
-        genre: this.genre.value,
-        completed: this.completed.value,
-        platform: {
-            id: this.platform.value
-            //name: this.platform.data
-        }
-    }
-    console.log(data);
-    axios.post("/games/create", data)
-        .then(res => {
-            getGames();
-            this.reset();
-            this.make.focus();
-        }).catch(err => console.log(err));
-});
 
 //READ
-const display = document.querySelector("#display")
+const display = document.querySelector("#display");
 const output = document.getElementById("output");//??
 const getGames = async () => {
-    const response = axios.get('/games');
-    display.innerHTML = "";
+    const response = await axios.get("/games/");
+    // display.innerHTML = "";
     output.innerHTML = ""; //??
+    console.log(response);
+    response.data.forEach(game => console.log(game));
     response.data.forEach(game => showGame(game));
 }
 
-const showGame = ({ id, name, platform }) => {
+const showGame = ({ id, name, genre, progress, platform }) => {
     const column = document.createElement("div");
     column.className = "col";
 
@@ -61,17 +42,25 @@ const showGame = ({ id, name, platform }) => {
 
     const completedText = document.createElement("p");
     completedText.className = "card-text";
-    completedText.innerText = `Status: ${completed}`;
+    completedText.innerText = `Status: ${progress}`;
     cardBody.appendChild(completedText);
 
-    const platformText = document.createElement("p");
-    platformText.className = "card-text";
-    platformText.innerText = `Platform: ${platform}`;
-    cardBody.appendChild(platformText);
+    // const platformText = document.createElement("p");
+    // platformText.className = "card-text";
+    // platformText.innerText = `Platform: ${platform}`;
+    // cardBody.appendChild(platformText);
 
     const cardFooter = document.createElement("div");
     cardFooter.className = "card-footer";
     card.appendChild(cardFooter);
+
+    const updateButton = document.createElement("a");
+    updateButton.innerText = "Update";
+    updateButton.className = "card-link";
+    updateButton.addEventListener("click", function () {
+        updateGame(id);
+    });
+    cardFooter.appendChild(updateButton);
 
     const deleteButton = document.createElement("a");
     deleteButton.innerText = "Delete";
@@ -84,19 +73,56 @@ const showGame = ({ id, name, platform }) => {
     output.appendChild(column);
 
 }
+getGames();
 
+//CREATE
+document.querySelector("#gameForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const data = {
+        name: this.gameName.value,
+        genre: this.genre.value,
+        progress: this.progress.value,
+        platform: {
+            id: this.platform.value,
+            //name: this.platform.data
+            name: "Steam"
+        }
+    }
+
+    console.log(data);
+    axios.post("/games/create/", data)
+        .then(res => {
+            getGames();
+            this.reset();
+            this.make.focus();
+        }).catch(err => console.log(err));
+});
 //UPDATE
-// var myModal = document.getElementById('myModal')
-// var myInput = document.getElementById('myInput')
 
-// myModal.addEventListener('shown.bs.modal', function () {
-//     myInput.focus()
-// })
+var myModal = new bootstrap.Modal(document.getElementById("gameModal"), {});
+const updateGame = async (id, data) => {
+    myModal.show();
+    // const response = await axios.put(`/games/update/${id}`, data);
+
+    getGames();
+}
+
+document.querySelector("#gameModal").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const data = {
+        name: this.gameName.value,
+        genre: this.genre.value,
+        progress: this.progress.value,
+    }
+    console.log(data);
+
+});
 
 
 //DELETE
 const deleteGame = async (id) => {
-    const res = await axios.delete(`/games/remove/${id}`);
+    const res = await axios.delete(`/games/delete/${id}`);
     getGames();
 };
 
