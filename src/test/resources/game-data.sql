be3d734c217b56564cdccb166524253adc7d32ab
Jamie Barnett <jamiebarnett44@gmail.com>
2021-06-17 10:01:35 +0100

    JB: Fixed GameIntegrationTest

--- src/test/resources/game-data.sql
+++ src/test/resources/game-data.sql
@@ -1,2 +1,2 @@
 insert into platform(name) values ('Steam');
-insert into game (genre, name, progress, platformid) values ('RPG', 'Bloodborne', 'Completed', 1);
+insert into game (genre, name, progress, platform_id) values ('RPG', 'Bloodborne', 'Completed', 1);
